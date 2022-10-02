49c81020f96e9eb0a1ee7428c9110d5779b26b3a
Quintin Willison <q@qwuk.net>
2022-10-02 17:31:11 +0100

    Make Tailwind CSS look at all the HTML documents generated.
    This had me puzzled for longer than I should admit.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  content: ['./output/*.html'],
+
+  // https://tailwindcss.com/docs/content-configuration#configuring-source-paths
+  content: [
+    './output/**/*.html', // the '**' matches zero or more directories
+  ],
+
   theme: {
     extend: {},
   },
